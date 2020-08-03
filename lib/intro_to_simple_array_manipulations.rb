4ebfed93a942684be216578a2ac3079481d53ab4
Dean Gelbaum <deanscottg@gmail.com>
2020-08-03 21:47:41 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,31 @@
+def using_concat (array, second_array)
+array.concat(second_array)
+array
+end
+
+def using_insert (array, new_element)
+array.insert(4, new_element)
+array
+  
+end
+
+def using_uniq (array)
+  array.uniq!()
+array
+end
+
+def using_flatten (array)
+array.flatten!()
+  array
+end
+
+def using_delete (array, string)
+array.delete(string)
+  array
+  
+  end
+  
+  def using_delete_at (array, integer)
+    array.delete_at(integer)
+    array
+  end 
